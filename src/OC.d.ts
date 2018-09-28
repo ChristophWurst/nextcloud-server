aa4f65ba6ef29528db43f97696217abb3f819923
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-28 14:03:56 +0200

    Add locale API
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/OC.d.ts
+++ src/OC.d.ts
@@ -2,4 +2,6 @@ declare namespace OC {
     let requestToken: String;
 
     function generateUrl(app: String, url: String, params: Object): String;
+
+    function getLocale():String;
 }
---
+++ src/l10n.ts
@@ -0,0 +1,8 @@
+/**
+ * Returns the user's locale
+ *
+ * @since 15.0.0
+ */
+export function getLocale(): String {
+    return OC.getLocale();
+}
