79e82ed92a64d7095aa24394a176e57991ecd043
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-28 16:44:22 +0200

    Make url parameters optional
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/OC.d.ts
+++ src/OC.d.ts
@@ -1,7 +1,7 @@
 declare namespace OC {
     let requestToken: String;
 
-    function generateUrl(app: String, url: String, params: Object): String;
+    function generateUrl(app: String, url: String, params?: Object): String;
 
     function getLocale(): String;
 
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 /**
  * @since 15.0.0
  */
-export function generateUrl(app: String, url: String, params: Object): String {
+export function generateUrl(app: String, url: String, params?: Object): String {
     return OC.generateUrl(app, url, params)
 }
